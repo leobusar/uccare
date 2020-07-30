4b12cfb69a645277ebf6b099d733dd820bd220e4
Leonardo Bustamante <leobusta@gmail.com>
2020-07-29 22:14:14 -0500

    uccare publish

---
+++ precache-manifest.4748804944f8964d34162e242331bfe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d1ce3c749bbaf72797c32e74826b3b",
+    "url": "/uccare/index.html"
+  },
+  {
+    "revision": "e1f1f09e23ec813ee0a6",
+    "url": "/uccare/static/js/2.f5577880.chunk.js"
+  },
+  {
+    "revision": "3af261872a18e5a486affef1268fc381",
+    "url": "/uccare/static/js/2.f5577880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695f45f50ba6b3859dbc",
+    "url": "/uccare/static/js/main.0c18a59a.chunk.js"
+  },
+  {
+    "revision": "c867e57745c89b09f15d",
+    "url": "/uccare/static/js/runtime-main.56623519.js"
+  }
+]);
